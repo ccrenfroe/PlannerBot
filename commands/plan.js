7c5b1d64113b22a8fc9d78078a6ceb0f4d1068a3
ccrenfroe <caleb.renfroe007@gmail.com>
2020-09-21 17:01:04 -0400

    Still learning async

--- commands/plan.js
+++ commands/plan.js
@@ -4,30 +4,19 @@ const Discord = require('discord.js')
 module.exports = {
 	name: 'plan',
     description: 'Create an embed for a newly planned event.\nFor event, choose \`game\` or \`other\`. For \`game\`, enter the full title of the game. For \`other\`, enter the chosen event name.\nNext enter a description.\nFinally, enter the time the event will take place.',
-    args:false, // CHANGE TO TRUE AFTER DONE
+    args:false,
     cooldown: 10,
     usage: '[Event] [Title] [Description] [Time]',
 	cooldown: 1,
     // The command operations go here.
     execute(message, args) 
     {
-        message.author.send("Lets get to work!\nPlease enter the title of your event. (Must be shorter than 200 characters)");        
-        var title = collector(message,20);
-        message.author.send("Please enter a short description of your event. (Must be shorter than 2000 characters)");        
-        var description = collector(message, 2000);
-
-        const eventEmbed = new Discord.MessageEmbed()
-        .setColor('RANDOM')
-        .setTitle(title)
-        .setAuthor(message.author.username)
-        .setDescription(description)
-        .setImage();
-        message.channel.send(eventEmbed);
+        embedBuilder(message);
     },
 };
 
 //TO-DO : Add a flag for a none option?
-function collector(message,limit) 
+async function collector(message,limit) 
 {
     message.channel.awaitMessages(response => response.author.id === message.author.id, 
         {
@@ -47,4 +36,19 @@ function collector(message,limit)
         .catch(() => {
             message.author.send("No message collected after 10 seconds.")
         })
+}
+
+async function embedBuilder(message)
+{
+    message.author.send("Lets get to work!\nPlease enter the title of your event. (Must be shorter than 200 characters)");
+    const title = await collector(message,200);
+    message.author.send("Please enter a short description of your event. (Must be shorter than 2000 characters)");
+    const description = await collector(message,2000);
+    const eventEmbed = new Discord.MessageEmbed()
+    .setColor('RANDOM')
+    .setTitle(title)
+    .setAuthor(message.author.username)
+    .setDescription(description)
+    .setImage();
+    message.channel.send(eventEmbed);
 }
